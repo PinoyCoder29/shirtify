b343fb297190d07b057160629524a8db5be0b994
PinoyCoder29 <bicojayvee4@gmail.com>
2026-06-14 16:44:01 +0800

    updated

---
+++ client/components/config/Mobilelink.ts
@@ -0,0 +1,4 @@
+export const Mobilelinks = [
+  { name: "product", path: "/product", icon: "" },
+  { name: "Home", path: "/", icon: "bi-house-door-fill" },
+];
---
+++ client/components/config/Navlink.ts
@@ -0,0 +1,10 @@
+export type Navlink = {
+  name: string;
+  path: string;
+  icon: string;
+};
+export const Navlinks: Navlink[] = [
+  { name: "Home", path: "/", icon: "" },
+  { name: "Product", path: "/product", icon: "" },
+  { name: "About us ", path: "/about", icon: "" },
+];
