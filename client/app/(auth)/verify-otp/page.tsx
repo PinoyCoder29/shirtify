c52485e6aba5a07b2e5a3a72268e485d1c4f7f5b
PinoyCoder29 <bicojayvee4@gmail.com>
2026-06-16 14:38:51 +0800

    updated

--- client/app/(auth)/verify-otp/page.tsx
+++ client/app/(auth)/verify-otp/page.tsx
@@ -3,9 +3,11 @@ export default function VerifyOtp() {
     <section className="p-5">
       <div className="container-fluid d-flex justify-content-center">
         <div className="row">
-          <div className="col-md-6 shadow">
-            <div className="col-md-6"></div>
-            <div className="col-md-6"></div>
+          <div className="col-md-6">
+            <div className="row shadow">
+              <div className="col-md-6">hi</div>
+              <div className="col-md-6"></div>
+            </div>
           </div>
         </div>
       </div>
