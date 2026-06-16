2cd91e3d5cea79de8cd1e37832cff84698c264f3
PinoyCoder29 <bicojayvee4@gmail.com>
2026-06-16 15:09:14 +0800

    updating services

--- client/services/api.ts
+++ client/services/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export async function apiPost(url: string, data: any) {
   const response = await fetch(`${BASE_URL}${url}`, {
     method: "POST",
